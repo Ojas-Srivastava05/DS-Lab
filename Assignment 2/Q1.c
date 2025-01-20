4617be02e3ec8450add4d05369035cf7b17b21a0
Ojas-Srivastava05 <srivastavaojas454@gmail.com>
2025-01-20 16:51:20 +0530

    .

--- Assignment 2/Q1.c
+++ Assignment 2/Q1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void input(int matrix1[3][3], int matrix2[3][3]);
 void product(int matrix1[3][3], int matrix2[3][3], int matrix3[3][3]);
@@ -62,9 +63,26 @@ void display(int matrix3[3][3])
 
 int main()
 {
-    int matrix1[3][3], matrix2[3][3], matrix3[3][3];
+    int **matrix1 = (int **)malloc(3 * sizeof(int *));
+    int **matrix2 = (int **)malloc(3 * sizeof(int *));
+    int **matrix3 = (int **)malloc(3 * sizeof(int *));
+    for (int i = 0; i < 3; i++)
+    {
+        matrix1[i] = (int *)malloc(3 * sizeof(int));
+        matrix2[i] = (int *)malloc(3 * sizeof(int));
+        matrix3[i] = (int *)malloc(3 * sizeof(int));
+    }
     input(matrix1, matrix2);
     product(matrix1, matrix2, matrix3);
     display(matrix3);
+    for (int i = 0; i < 3; i++)
+    {
+        free(matrix1[i]);
+        free(matrix2[i]);
+        free(matrix3[i]);
+    }
+    free(matrix1);
+    free(matrix2);
+    free(matrix3);
     return 0;
 }
--- Assignment 2/Q2.c
+++ Assignment 2/Q2.c
@@ -1,30 +1,45 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
 struct question2
 {
-  int *ptr1;
-  int *ptr2;
-}ques;
+    int *ptr1;
+    int *ptr2;
+} ques;
+
 int main()
 {
     int n1;
     printf("ENTER THE NUMBER OF ELEMENTS IN ARRAY ");
-    scanf("%d",&n1);
-    int arr1[n1];
-    int arr2[n1];
-    ques.ptr1=&arr1[0];
-    ques.ptr2=&arr2[0];
+    scanf("%d", &n1);
+
+    int *arr1 = (int *)malloc(n1 * sizeof(int));
+    int *arr2 = (int *)malloc(n1 * sizeof(int));
+
+    if (arr1 == NULL || arr2 == NULL)
+    {
+        printf("Memory allocation failed!");
+        return 1;
+    }
+
+    ques.ptr1 = arr1;
+    ques.ptr2 = arr2;
+
     printf("ENTER THE ELEMENTS OF ARRAY ");
-    for(int i=0;i<n1;i++)
+    for (int i = 0; i < n1; i++)
     {
-        scanf("%d",ques.ptr1+i);
+        scanf("%d", ques.ptr1 + i);
     }
-    for(int i=0;i<n1;i++)
+
+    for (int i = 0; i < n1; i++)
     {
-        *(ques.ptr2+i)=*(ques.ptr1+i);
+        *(ques.ptr2 + i) = *(ques.ptr1 + i);
     }
-    for(int i=0;i<n1;i++)
+
+    for (int i = 0; i < n1; i++)
     {
-        if(i%2==1)
+        if (i % 2 == 1)
         {
             if (i > 0 && i < n1 - 1)
             {
@@ -32,10 +47,14 @@ int main()
             }
         }
     }
-    for(int i=0;i<n1;i++)
+
+    for (int i = 0; i < n1; i++)
     {
-        printf("%d ",*(ques.ptr2+i));
+        printf("%d ", *(ques.ptr2 + i));
     }
 
+    free(arr1);
+    free(arr2);
 
+    return 0;
 }
