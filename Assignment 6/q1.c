1e9c2b01d019f020918be0445b1f016eef98c7b8
Ojas-Srivastava05 <srivastavaojas454@gmail.com>
2025-02-15 12:15:44 +0530

    Update q1.c

--- Assignment 6/q1.c
+++ Assignment 6/q1.c
@@ -1,44 +1,69 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<conio.h>
-struct stack{
-    char data;
-    struct stack*next;
-};
-void main()
-{
-    struct stack *A;
-    char exp[100];
-    printf("Enter the expression ");
-    scanf("%s",exp);
-    createstack(&A);
-}
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
-void createstack(struct stack **top)
-{
-    *top=NULL;
-}
+#define MAX 100
 
-int postfix(char exp)
-{
-    struct stack *A;
-    
-}
+char stack[MAX];
+int top = -1;
 
-void push(struct stack **top,int num)
-{
-    struct stack *new=(struct stack*)malloc(sizeof(struct stack));
-    if(new==NULL)
-    {
-        printf("\n Stack is full");
-        exit(-1);
+void push(char c) {
+    if (top < MAX - 1) {
+        stack[++top] = c;
     }
-
-
-
 }
 
-int pop(){
-
+char pop() {
+    if (top >= 0) {
+        return stack[top--];
+    }
+    return '#';
 }
 
+char peek() {
+    if (top >= 0) {
+        return stack[top];
+    }
+    return '#';
+}
+
+int precedence(char c) {
+    if (c == '+' || c == '-') return 1;
+    if (c == '*' || c == '/') return 2;
+    return 0;
+}
+
+void infixToPostfix(char *infix, char *postfix) {
+    int i, j = 0;
+    for (i = 0; infix[i] != '\0'; i++) {
+        if (isalnum(infix[i])) {
+            postfix[j++] = infix[i];
+        } else if (infix[i] == '(') {
+            push(infix[i]);
+        } else if (infix[i] == ')') {
+            while (top != -1 && peek() != '(') {
+                postfix[j++] = pop();
+            }
+            pop();
+        } else {
+            while (top != -1 && precedence(peek()) >= precedence(infix[i])) {
+                postfix[j++] = pop();
+            }
+            push(infix[i]);
+        }
+    }
+    while (top != -1) {
+        postfix[j++] = pop();
+    }
+    postfix[j] = '\0';
+}
+
+int main() {
+    char infix[MAX], postfix[MAX];
+    printf("Enter infix expression: ");
+    scanf("%s", infix);
+    infixToPostfix(infix, postfix);
+    printf("Postfix expression: %s\n", postfix);
+    return 0;
+}
